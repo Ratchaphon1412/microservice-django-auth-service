60408e66766844dd7a60b93210f48e7497462ef9
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-05 22:50:07 +0700

    create api register and login refresh token jwt

--- User/urls.py
+++ User/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from .views import RegisterAPI
+
 urlpatterns = [
     path('user/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', RegisterAPI.as_view(), name='register'),
 ]
--- User/views.py
+++ User/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, mixins
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, UserSerializer
+
+
+#Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "user":UserSerializer(serializer.instance).data,
+                "message":"User Created Successfully.  Now perform Login to get your token",
+            })
+        else:
+            return Response({
+                "user":serializer.errors,
+                "message":"User Creation Failed",
+            })
+
 
 # Create your views here.
