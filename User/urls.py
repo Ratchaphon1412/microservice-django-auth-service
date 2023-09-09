bb9431c06d352c6d2dffae27f3ab752f24a6f394
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-09 16:31:41 +0700

    add route test kafka

--- User/urls.py
+++ User/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from .views import RegisterAPI
+from .views import *
 
 urlpatterns = [
     path('user/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', RegisterAPI.as_view(), name='register'),
+    path('user/test/kafka', TestSendTOKafka.as_view(), name='test_kafka')
 ]
