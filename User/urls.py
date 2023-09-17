3874e5b8b2ec77183256c7fe8dd601c8503e35e9
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-17 15:53:57 +0700

    edit route api user

--- User/urls.py
+++ User/urls.py
@@ -4,8 +4,10 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import *
 
 urlpatterns = [
-    path('user/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('user/register/', RegisterAPI.as_view(), name='register'),
-    path('user/test/kafka', TestSendTOKafka.as_view(), name='test_kafka')
+    path('user/register/', RegisterUserAPI.as_view(), name='register'),
+    path('user/update/', UpdateUserAPI.as_view(), name='update'),
+    path('user/delete/', DeleteUserAPI.as_view(), name='delete'),
+  
 ]
