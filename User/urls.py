0f3760604c6579cf977b6b9f1ffc14cfaae22a80
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-17 19:41:57 +0700

    edit route login

--- User/urls.py
+++ User/urls.py
@@ -4,10 +4,10 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import *
 
 urlpatterns = [
-    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/login/',  LoginAPIView.as_view(), name='token_obtain_pair'),
     path('user/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', RegisterUserAPI.as_view(), name='register'),
     path('user/update/', UpdateUserAPI.as_view(), name='update'),
     path('user/delete/', DeleteUserAPI.as_view(), name='delete'),
-  
+    path('user/', GetUserAPI.as_view(), name='user'),
 ]
