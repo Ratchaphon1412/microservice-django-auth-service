ff6cc7de5145ca554f85298895b9f325388629cf
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-17 19:39:36 +0700

    inheritan permissionmixin to user model

--- User/models.py
+++ User/models.py
@@ -1,8 +1,10 @@
+from typing import Any
 import uuid
 from django.db import models
 
 
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import UserManager
 from django.db import models
 
@@ -12,14 +14,28 @@ from django.db import models
 
 
 class UserProfilesManager(UserManager):
-    pass
-    # def get_by_natural_key(self, username):
-    #         return super().get_by_natural_key(username)
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a email')
+        
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.is_email_verified = False
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(self._db)
+        return user
+        
+        
+    def get_by_natural_key(self, username):
+            return super().get_by_natural_key(username)
     
     
 
 # Create your models here
-class UserProfiles(AbstractBaseUser):
+class UserProfiles(AbstractBaseUser,PermissionsMixin):
     MALE = 'Male'
     FEMALE = 'Female'
     GENDER_IN_CHOICES = [
@@ -36,6 +52,10 @@ class UserProfiles(AbstractBaseUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
     gender = models.CharField(max_length=6, choices=GENDER_IN_CHOICES, null=True, blank=True)
     is_email_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = UserProfilesManager()
     
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
