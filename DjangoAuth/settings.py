ffcc31c9519915841aea960da3fa2695f101b7f7
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-10-17 02:23:58 +0700

    change name templates folder

--- DjangoAuth/settings.py
+++ DjangoAuth/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "DjangoAuth.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [ os.path.join(BASE_DIR, 'Templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
