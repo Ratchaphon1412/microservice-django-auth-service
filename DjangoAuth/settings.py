a1bca7daf787ee463b930192d08667b67700a8c2
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-04 16:33:36 +0700

    spacific cors

--- DjangoAuth/settings.py
+++ DjangoAuth/settings.py
@@ -31,7 +31,18 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOW_ALL_ORIGINS =True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 
 
 # Application definition
