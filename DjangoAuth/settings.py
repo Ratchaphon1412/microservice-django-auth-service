f36412d8d2f151f3de8e29c9ab9d176cbf556a3b
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-04 17:51:30 +0700

    test allowed origin

--- DjangoAuth/settings.py
+++ DjangoAuth/settings.py
@@ -31,7 +31,11 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['microservice.auth.ratchaphon1412.co']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = (
