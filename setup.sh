7222fde045423c57a7ccbd20dd08ed6f2f4c3054
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-06 07:41:09 +0700

    test script setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 poetry run python3 manage.py collectstatic --noinput
 poetry run python3 manage.py makemigrations 
 poetry run python3 manage.py migrate
-poetry run python3 manage.py ConsumerCommand --topic payment_customer --group auth 
 poetry run gunicorn --bind 0.0.0.0:8024 DjangoAuth.wsgi
+poetry run python3 manage.py ConsumerCommand --topic payment_customer --group auth 
+
 
